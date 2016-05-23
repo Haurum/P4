fad1b6208f6e6e1a17c9bd30cd74f898b7ac4b01
Mark Kloch Haurum <mark@markhaurum.com>
2016-05-23 09:19:25 +0200

    Testing

---
+++ Grammar/test/MainTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by haurum on 23/05/2016.
+ */
+public class MainTest {
+    @Test
+    public void main() {
+
+    }
+
+    @Test
+    public void importReservedFunctions() {
+
+    }
+
+}

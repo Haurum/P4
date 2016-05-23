4334ca8111ec65696b6b4056aa74a77690ed568d
Mark Kloch Haurum <mark@markhaurum.com>
2016-05-23 13:21:19 +0200

    All them testrobots

---
+++ Grammar/testrobots/SameActionFunctionName.java
@@ -0,0 +1,11 @@
+import robocode.*;
+import static robocode.util.Utils.*;
+public class RamFire extends Robot {
+ double _turnDirection = 1;
+public void run(){
+  
+while (true){
+ turnRight(5 * _turnDirection);
+ 
+}
+}}
---
+++ Grammar/testrobots/SubtractionNumString.java
@@ -0,0 +1,12 @@
+import robocode.*;
+import static robocode.util.Utils.*;
+public class RamFire extends Robot {
+ double _turnDirection = 1;
+public void run(){
+ out.println(5 + "hej");
+ 
+while (true){
+ turnRight(5 * _turnDirection);
+ 
+}
+}}
